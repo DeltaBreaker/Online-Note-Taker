a94fefdef7e5d31d20222aa4d1723a9b574b223c
willc <willcrain1@gmail.com>
2022-10-28 12:44:06 -0400

    Created router file for /notes

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
+const notesRouter = require("./notes.js");
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api/notes", notesRouter);
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/404.html'))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () =>
-  console.log(`Listening for requests.`)
+  console.log("Listening for requests.")
 );
