b0ad7d7de724f2b240f6e2087fb0c600d5bb5871
willc <willcrain1@gmail.com>
2022-10-27 20:08:01 -0400

    Created base server code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Listening for requests.`)
+);
