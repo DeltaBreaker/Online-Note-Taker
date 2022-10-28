a1eea434c43c56d77d1d0dac9867701705ce3e70
willc <willcrain1@gmail.com>
2022-10-28 12:45:16 -0400

    Notes can now be saved and loaded from db.json

--- notes.js
+++ notes.js
@@ -1,4 +1,23 @@
+const fs = require("fs");
+const uuid = require("uuid");
 const notes = require('express').Router();
+const noteDB = require("./db/db.json");
+const notesArray = [];
 
+for(const i of noteDB) {
+    notesArray.push(i);
+}
+
+notes.post('/', (req,res) => {
+    notesArray.push({
+        id: uuid.v4(),
+        title: req.body.title,
+        text: req.body.text
+    });
+    fs.writeFile("./db/db.json", JSON.stringify(notesArray), (error) => {
+        (error) ? console.log("Error writing to file.") : console.log("Notes db updated.");
+    });
+    res.sendStatus(200);
+});
 
 module.exports = notes;
