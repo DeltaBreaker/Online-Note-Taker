bfcefb9a3f2b497159f15477a008451c14d470f3
willc <willcrain1@gmail.com>
2022-10-28 03:09:25 -0400

    Added a catch all 404 page

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.port || 3001;
 
@@ -7,8 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/404.html'))
 );
 
 app.listen(PORT, () =>
