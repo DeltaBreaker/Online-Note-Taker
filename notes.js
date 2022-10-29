9bb36c794bbe94b2567914ca537264900405153d
willc <willcrain1@gmail.com>
2022-10-28 22:35:06 -0400

    Added comments to js files

--- notes.js
+++ notes.js
@@ -1,17 +1,25 @@
+//----- Import needed modules -----
 const fs = require("fs");
 const uuid = require("uuid");
 const notes = require('express').Router();
+//---------------------------------
+
+//----- Loads the db.json file into an array to use for storing notes -----
 const noteDB = require("./db/db.json");
 const notesArray = [];
 
 for(const i of noteDB) {
     notesArray.push(i);
 }
+//-------------------------------------------------------------------------
 
+//---------- Create endpoints for the /api/notes route ----------
+// Returns the list of notes
 notes.get('/', (req,res) => {
     res.send(JSON.stringify(notesArray));
 });
 
+// Takes in a note object and appends that to the array. Updates the json file after
 notes.post('/', (req,res) => {
     notesArray.push({
         id: uuid.v4(),
@@ -24,6 +32,7 @@ notes.post('/', (req,res) => {
     res.sendStatus(200);
 });
 
+// Takes in a note id parameter and uses that to delete a specific note
 notes.delete("/:id", (req, res) => {
     for(let i = 0; i < notesArray.length; i++) {
         if(notesArray[i].id == req.params.id) {
@@ -33,5 +42,6 @@ notes.delete("/:id", (req, res) => {
     }
     res.sendStatus(200);
 });
+//--------------------------------------------------------------
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+//---------- Import all needed modules ----------
 const express = require("express");
 const path = require("path");
 const notesRouter = require("./notes.js");
+//-----------------------------------------------
 
+//----- Create the port and express variabled needed -----
 const PORT = process.env.port || 3001;
-
 const app = express();
+//--------------------------------------------------------
 
-
+//----- Set up the server abilities and apply custom route -----
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api/notes", notesRouter);
+//--------------------------------------------------------------
 
+//----- Create endpoints for html files -----
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
@@ -19,7 +24,9 @@ app.get('/notes', (req,res) => {
 app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+//-------------------------------------------
 
+//----- Start listening for requests -----
 app.listen(PORT, () =>
   console.log("Listening for requests.")
 );
